4a645648f0092cba1fdea5c99108447d1fd97a97
NISHIDO-LAPTOP\admin <quan5555qt@gmail.com>
2021-11-15 11:18:10 +0700

    Upload template

---
+++ laravelPHP/app/Http/Controllers/API/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller{
+    //
+    public function login(Request $request){
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        $user = DB::table('User')
+                ->where('username',$username)
+                ->where('password',$password)->get();
+
+        if ($user == NULL){
+            return response()->json([
+                'status' => 404,
+                'message' => 'Login failed!',
+            ]);
+        }
+        else {
+            return response()->json([
+                'status' => 200,
+                'user' => $user,
+                'message' => 'Login Successfully',
+            ]);
+        }
+        
+    }
+}
--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*!
+
+=========================================================
+* Purity UI Dashboard - v1.0.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/purity-ui-dashboard
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/purity-ui-dashboard/blob/master/LICENSE.md)
+
+* Design by Creative Tim & Coded by Simmmple
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import AuthLayout from "layouts/Auth.js";
+import AdminLayout from "layouts/Admin.js";
+import RTLLayout from "layouts/RTL.js";
+import axios from 'axios';
+axios.defaults.baseURL = "http://localhost:8000/";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <HashRouter>
+    <Switch>
+      <Route path={`/auth`} component={AuthLayout} />
+      <Route path={`/admin`} component={AdminLayout} />
+      <Route path={`/rtl`} component={RTLLayout} />
+      <Redirect from={`/`} to="/admin/dashboard" />
+    </Switch>
+  </HashRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
